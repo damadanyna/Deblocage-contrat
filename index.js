734d67e4a11fdb42edde070f8e8afa586eca36b9
Dama <damadanyna@gmail.com>
2024-06-12 21:35:41 +0300

    finition du disigne

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const ip_= document.getElementById('ip_');
-const agence_=document.getElementById('agence_');
-const code_=document.getElementById('code_');
-const btn=document.getElementById('btn_');
+const ip_ = document.getElementById('ip_');
+const agence_ = document.getElementById('agence_');
+const code_ = document.getElementById('code_');
+const btn = document.getElementById('btn_');
 
-function checkInput(){
-    if (ip_.value=="" || agence_.value=="" || code_.value=="") {
-        btn.style.background="#555";
+function checkInput() {
+    if (ip_.value == "" || agence_.value == "" || code_.value == "") {
+        btn.style.background = "#555";
     } else {
-        if (code_.value.length>6) {
-             btn.style.background='#40c240'
+        if (code_.value.length > 6) {
+            btn.style.background = '#0b773b'
             appelerFonctionPHP();
             // window.location.href = "./controller/getAccount.php";
         }
@@ -17,13 +17,13 @@ function checkInput(){
 function appelerFonctionPHP() {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', './controller/getAccount.php?action=test', true);
-    
-    xhr.onload = function() {
+
+    xhr.onload = function () {
         if (xhr.status === 200) {
             console.log('Réponse du serveur : ' + xhr.responseText);
             // Traitez la réponse ici
         }
     };
-    
+
     xhr.send();
 }
